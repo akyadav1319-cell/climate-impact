1bc349c5ba438b1a618eef7d2228417b353f289f
akyadav1319-cell <ak.yadav1319@gmail.com>
2026-02-08 00:37:11 +0530

    Add dark mode class to document element

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,9 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './app/routes';
 import { SimulationProvider } from './app/context/SimulationContext';
 
+
 import './styles/index.css';
+document.documentElement.classList.add('dark');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
